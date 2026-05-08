80f8ea1e43862b29e5ac5f676f2509de1a0a5ad8
Krishanu Mishra <2401001352@cgcjhanjeri.in>
2026-05-09 02:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (54.58%) - LeetSync

---
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        
+                int xo = 0;
+        for (int i = 0; i < n; i++) {
+            xo ^= (start + 2 * i);
+        }
+        return xo;
+
+    }
+};
