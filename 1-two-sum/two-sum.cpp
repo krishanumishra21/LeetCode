14e6589288d039d12c76104643d4462e92857e9a
Krishanu Mishra <2401001352@cgcjhanjeri.in>
2026-06-01 14:51:58 +0530

    Time: 130 ms (5.08%) | Memory: 14.1 MB (68.86%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,7 @@ public:
     int n =nums.size();
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            if(nums[i]+nums[j]==target){      
+            if(nums[i]+nums[j]==target){
                  ans.push_back(i);
                  ans.push_back(j);
 
